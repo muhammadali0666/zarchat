12d70c3a7599d245fa8da60166d7adfe04cd155e
muhammadali0666 <muhammadalishuhratjonov50@gmail.com>
2024-06-29 20:19:02 +0500

    added left bar

--- src/components/chat/ChatList/chatList.jsx
+++ src/components/chat/ChatList/chatList.jsx
@@ -1,15 +1,57 @@
-import "./chatList.css"
+import { useState } from "react";
+import "./chatList.css";
 
 export const ChatList = () => {
+  const [addMode, setAddMode] = useState(false);
   return (
     <div className="chatList">
       <div className="search">
         <div className="searchBar">
-<img src="./search.png" alt="" />
-<input type="text" placeholder="Search..." />
+          <img src="./search.png" alt="" />
+          <input type="text" placeholder="Search..." />
         </div>
-        <img src="./plus.png" alt="" />
+        <img
+          src={addMode ? "./minus.png" : "./plus.png"}
+          alt=""
+          className="add"
+          onClick={() => setAddMode((prev) => !prev)}
+        />
+      </div>
+      <div className="item">
+        <img src="/avatar.png" alt="" />
+       <div className="texts">
+       <span>Jane Doe</span>
+       <p>Hello</p>
+       </div>
+      </div>
+      <div className="item">
+        <img src="/avatar.png" alt="" />
+       <div className="texts">
+       <span>Jane Doe</span>
+       <p>Hello</p>
+       </div>
+      </div>
+      <div className="item">
+        <img src="/avatar.png" alt="" />
+       <div className="texts">
+       <span>Jane Doe</span>
+       <p>Hello</p>
+       </div>
+      </div>
+      <div className="item">
+        <img src="/avatar.png" alt="" />
+       <div className="texts">
+       <span>Jane Doe</span>
+       <p>Hello</p>
+       </div>
+      </div>
+      <div className="item">
+        <img src="/avatar.png" alt="" />
+       <div className="texts">
+       <span>Jane Doe</span>
+       <p>Hello</p>
+       </div>
       </div>
     </div>
-  )
-}
+  );
+};
--- src/components/chat/chat.jsx
+++ src/components/chat/chat.jsx
@@ -1,7 +1,58 @@
-import "./chat.css"
+import { useState } from "react";
+import "./chat.css";
+import EmojiPicker from "emoji-picker-react";
 
 export const Chat = () => {
+  const [open, setOpen] = useState(false);
+  const [text, setText] = useState("");
+
+  const handleEmoji = (e) => {
+    setText((prev) => prev + e.emoji);
+    setOpen(false);
+  };
+
   return (
-    <div className="chat">chat</div>
-  )
-}
+    <div className="chat">
+      <div className="top">
+        <div className="user">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <span>Jane Doe</span>
+            <p>Lorem, ipsum dolor.</p>
+          </div>
+        </div>
+        <div className="icons">
+          <img src="./phone.png" alt="" />
+          <img src="./video.png" alt="" />
+          <img src="./info.png" alt="" />
+        </div>
+      </div>
+      <div className="center"></div>
+      <div className="bottom">
+        <div className="icons">
+          <img src="./img.png" alt="" />
+          <img src="./camera.png" alt="" />
+          <img src="./mic.png" alt="" />
+        </div>
+        <input
+          type="text"
+          placeholder="Type a message..."
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <div className="emoji">
+          <img
+            src="./emoji.png"
+            alt=""
+            className="emoji"
+            onClick={() => setOpen((prev) => !prev)}
+          />
+          <div className="picker">
+          <EmojiPicker open={open} onEmojiClick={handleEmoji} />
+          </div>
+        </div>
+        <button className="sendButton">Send</button>
+      </div>
+    </div>
+  );
+};
